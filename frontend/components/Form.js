f9a9d72716438d0635a01d5d73a8bbb26f9d51ba
unknown <monk.sound.design@gmail.com>
2023-06-16 16:35:50 -0400

    handleChange

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,15 +2,29 @@ import React from 'react'
 
 export default class Form extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    }
+  }
+
   handleSubmit = (e) => {
     e.preventDefault()
-    this.props.add()
+    this.props.add(this.state.input)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+    })
   }
 
   render() {
     return (
       <form onClick={this.handleSubmit}>
-        <input/>
+        <input onChange={handleChange}/>
         <button>Add</button>
       </form>
     )
